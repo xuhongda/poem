46d389e1b3d069b25ef4a68a377b9ad468ed0edc
xuhongda <xuhongda7@outlook.com>
2020-05-22 16:25:54 +0800

    update

---
+++ about/java/main/Xuhongda.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.io.*;
+
+/**
+ * @author xuhongda on 2020/5/21
+ * main
+ * poem
+ */
+public class Xuhongda {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("xuhongda");
+    }
+}
